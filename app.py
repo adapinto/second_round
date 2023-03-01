af9e6b0c4f6ac005765e0f56fedcc544bd6c2169
Ada Pinto <ada.pinto@unah.hn>
2023-02-28 22:40:05 -0600

    DbMongo

--- app.py
+++ app.py
@@ -1,39 +1,44 @@
 import pymongo
 from pymongo import MongoClient
-from classes import DATA, Dataprocess,Students
+from classes import DATA, Dataprocess,Students,DbMongo
+from dotenv import load_dotenv
 
 def main():
-    user = 'Adimari'
-    password = '99725478Adimari'
-    cluster = 'poounahclase3.v7p4vxg.mongodb.net'
-    query_string = 'retryWrites=true&w=majority'
-
-
-    ## Connection String
-    uri = "mongodb+srv://{0}:{1}@{2}/?{3}".format(
-        user
-        , password
-        , cluster
-        , query_string
-    )
-
-    client = pymongo.MongoClient(uri)
-    db = client['courses-careers-students']
+    client, db = DbMongo.getDB()
+   # user = 'Adimari'
+   # password = '99725478Adimari'
+   # cluster = 'poounahclase3.v7p4vxg.mongodb.net'
+   # query_string = 'retryWrites=true&w=majority'
+#
+#
+   # ## Connection String
+   # uri = "mongodb+srv://{0}:{1}@{2}/?{3}".format(
+   #     user
+   #     , password
+   #     , cluster
+   #     , query_string
+   # )
+#
+   #client = pymongo.MongoClient(uri)
+   #db = client['courses-careers-students']
     collection = db['students']
-    
-    #Elina todo lo que hay en la db para que no dupliquen los documentos
+   
+   # #Elina todo lo que hay en la db para que no dupliquen los documentos
     delete_all = collection.delete_many({})
-
-    #Añadir la data a la coleccion de estudiantes
-    collection.insert_many(DATA)
+#
+   # #Añadir la data a la coleccion de estudiantes
+   # collection.insert_many(DATA)
     
     pipeline = Dataprocess(DATA)
     
+    pipeline.create_students(db)
     pipeline.create_careers()
-    pipeline.create_students()
     pipeline.create_enrollments()
-    
-    return True
 
-if __name__ == "__main__":
-    main()
+    
+
+    client.close()
+        
+    if __name__ == "__main__":
+        load_dotenv()
+        main()
--- classes/Dataprocess.py
+++ classes/Dataprocess.py
@@ -1,13 +1,24 @@
+import pymongo
+from dotenv import load_dotenv
+from classes import DbMongo,DATA
+
+
+
 class Dataprocess:
 
-    def __init__(self, data):
+      def __init__(self, data):
         self.__data = data
 
-    def create_careers(self,nombre,id):
-        return nombre, id
-    def create_courses(self,nombre,id):
-        return nombre,id
-    def create_students(self,numero_cuenta,nombre_completo,cursos_aprobados,cursos_reprobados,edad,carrera,id):
-        return numero_cuenta,nombre_completo,cursos_aprobados,cursos_reprobados,edad,carrera,id
-    def create_enrollments(self,alumno,curso,estado):
-        return alumno,curso,estado
+      def create_careers(self,db):
+          collection = db['careers']
+          return True
+      def create_courses(self,db):
+          collection = db['courses']
+          return True
+      def create_students(self,db):
+          collection = db['students']
+          collection.insert_many(DATA)
+          return True
+      def create_enrollments(self,db):
+          collection = db['enrollments']
+          return True
